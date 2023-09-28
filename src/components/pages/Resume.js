4831549d5b8155bf19a3bdd65af5285007780781
fzhao888 <frank.zhao93@gmail.com>
2023-09-27 19:10:14 -0500

    Added PortfolioContainer and placeholder code for Header,Footer,and Navigation

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,7 +9,7 @@ export default function Resume() {
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
         mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
         lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cufrsus
         in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
       </p>
     </div>
