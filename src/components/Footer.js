3ba50f13aed0e69d7a4fea2700e300f95060405c
fzhao888 <frank.zhao93@gmail.com>
2023-09-27 20:22:17 -0500

    Added github and linkedin icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'; 
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
 
 function Footer(){
     return (
-        <div>
-            @ Frank Zhao 2023
-        </div>
+        <section class="card">
+        <ul class="links"> 
+            <a href="https://github.com/fzhao888"> <BsGithub/></a>
+            <a href="https://www.linkedin.com/in/frank-zhao-1bb65822a/"><BsLinkedin/></a>
+           
+        </ul>
+      </section>
     );
 };
 
