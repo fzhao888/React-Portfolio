82c9030a91d9614852e80dcb21df48e94c9a654d
fzhao888 <frank.zhao93@gmail.com>
2023-09-27 19:09:38 -0500

    Added lorem ipsum for about, contact, portfolio, and resume pages

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navigation({ currentPage, handlePageChange }){
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Navigation;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'; 
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Header from './Header';
+import Navigation from './Navigation'; 
+import Footer from './Footer';
+
+
+export default function PortfolioContainer() {
+
+    const[currentPage, setCurrentPage] = useState('About');
+
+    // checks value of currentPage, then renders the corresponding component
+    const renderPage = () => {
+        if( currentPage === 'About' ) {
+            return <About />;
+        }
+
+        if( currentPage === 'Portfolio' ){
+            return <Portfolio />;
+        }
+
+        if( currentPage === 'Contact' ) {
+            return <Contact />;
+        }
+
+        if( currentPage === 'Resume' ){
+            return <Resume />;
+        } 
+    }
+
+    // sets current page
+    const handlePageChange = (page) => setCurrentPage(page);
+
+
+    return (
+        <div>
+            <Header/>
+
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
+            {renderPage()}
+
+            <Footer />
+        </div>
+    );
+
+
+}
