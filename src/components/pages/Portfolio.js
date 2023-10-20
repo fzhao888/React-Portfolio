c1fd438609d7c1b36fb6b35854d43978038a58d3
fzhao888 <frank.zhao93@gmail.com>
2023-10-19 19:11:34 -0500

    Added DnD Commerce to Portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,52 +1,51 @@
-import React from 'react';
-import Project from '../Project'; 
+import React from "react";
+import Project from "../Project";
 
 // an array of projects -> each project has a title, github link, image link, deployed link
 const projects = [
   {
-      title: 'ReelDeel',
-      imageLink: 'images/search-results.png',
-      GitHubLink: 'https://github.com/fzhao888/ReelDeal',
-      deployedLink: 'https://fzhao888.github.io/ReelDeal/', 
+    title: "DnD Commerece",
+    imageLink: "images/DnD-storefront.gif",
+    GitHubLink: "https://github.com/fzhao888/DNDCommerce",
+    deployedLink: "https://frank-dnd-commerce-80c31f1850fc.herokuapp.com/",
+  },
+  {
+    title: "Lil Chef",
+    imageLink: "images/recipesList.png",
+    GitHubLink: "https://github.com/fzhao888/Little-Chef",
+    deployedLink: "https://remys-little-chef-64326918dfaf.herokuapp.com/",
+  },
+  {
+    title: "ReelDeel",
+    imageLink: "images/search-results.png",
+    GitHubLink: "https://github.com/fzhao888/ReelDeal",
+    deployedLink: "https://fzhao888.github.io/ReelDeal/",
   },
 
   {
-      title: 'Lil Chef',
-      imageLink: 'images/recipesList.png',
-      GitHubLink: 'https://github.com/fzhao888/Little-Chef',
-      deployedLink: 'https://remys-little-chef-64326918dfaf.herokuapp.com/', 
-  },
-
-  {
-      title: 'TechBlog',
-      imageLink: 'images/techblog.png',
-      GitHubLink: 'https://github.com/fzhao888/Tech-Blog',
-      deployedLink: 'https://frank-tech-blog-045fc2396261.herokuapp.com/', 
+    title: "TechBlog",
+    imageLink: "images/techblog.png",
+    GitHubLink: "https://github.com/fzhao888/Tech-Blog",
+    deployedLink: "https://frank-tech-blog-045fc2396261.herokuapp.com/",
   },
   {
-      title: 'NoteTaker',
-      imageLink: 'images/notetaker_sample2.png',
-      GitHubLink: 'https://github.com/fzhao888/Note-Taker',
-      deployedLink: 'https://frank-note-taker-e1c255ad5ce5.herokuapp.com/', 
+    title: "NoteTaker",
+    imageLink: "images/notetaker_sample2.png",
+    GitHubLink: "https://github.com/fzhao888/Note-Taker",
+    deployedLink: "https://frank-note-taker-e1c255ad5ce5.herokuapp.com/",
   },
   {
-      title: 'Work Day Scheduler',
-      imageLink: 'images/Work-Day-Scheduler-Sample.png',
-      GitHubLink: 'https://github.com/fzhao888/Work-Day-Scheduler',
-      deployedLink: 'https://fzhao888.github.io/Work-Day-Scheduler/', 
+    title: "Work Day Scheduler",
+    imageLink: "images/Work-Day-Scheduler-Sample.png",
+    GitHubLink: "https://github.com/fzhao888/Work-Day-Scheduler",
+    deployedLink: "https://fzhao888.github.io/Work-Day-Scheduler/",
   },
-  {
-      title: 'Weather Dashboard',
-      imageLink: 'images/Weather-Dashboard.png',
-      GitHubLink: 'https://github.com/fzhao888/Weather-Dashboard',
-      deployedLink: 'https://fzhao888.github.io/Weather-Dashboard/', 
-  }
-]
+];
 
 export default function Portfolio() {
   return (
     <div>
-      <section className='portfolio'> 
+      <section className="portfolio">
         <Project projects={projects} />
       </section>
     </div>
