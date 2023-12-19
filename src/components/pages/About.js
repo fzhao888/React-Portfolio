746f6ca752ee9090aece2a173b12dc2a7f3f76f8
fzhao888 <frank.zhao93@gmail.com>
2023-12-18 23:19:46 -0600

    Resized about me picture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <h1>About</h1>
       <section className='me'>
         <section class='meImg'>
-          <img src="images/me.jpeg" alt="image of me" className='img-fluid rounded' />
+          <img src="images/me_site.jpg" alt="image of me" className='img-fluid rounded' />
         </section>
         <p>
           Hello! My name is Frank Zhao.  I recently graduated bootcamp class to become a fullstack developer, and also have a Bachelor's in Computer Science.  This webpage showcases all the projects I have worked on.
