3ca0250aea54671821ebb8b985d08d857f31552d
fzhao888 <frank.zhao93@gmail.com>
2023-12-18 23:03:37 -0600

    Updated about professional pic, about description, and resume

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,10 +6,10 @@ export default function About() {
       <h1>About</h1>
       <section className='me'>
         <section class='meImg'>
-          <img src="images/me.png" alt="image of me" className='img-fluid rounded' />
+          <img src="images/me.jpeg" alt="image of me" className='img-fluid rounded' />
         </section>
         <p>
-          Hello! My name is Frank Zhao.  I am currently taking a bootcamp class to become a fullstack developer.  This webpage showcases all the projects I have worked on.
+          Hello! My name is Frank Zhao.  I recently graduated bootcamp class to become a fullstack developer, and also have a Bachelor's in Computer Science.  This webpage showcases all the projects I have worked on.
         </p>
       </section>
     </div>
